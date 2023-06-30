bbe382624a12ec03acf6f1a7f4552e6dd9f7d460
h-spinde <hannah.spinde@st.ovgu.de>
2023-06-30 18:19:58 +0200

    Fixing Syntax Errors

--- PrimeCheckTest.java
+++ PrimeCheckTest.java
@@ -22,16 +22,16 @@ public class PrimeCheckTest {
   // --------------------------------------------------------------- //
   @Test
   public void testPrimes() {
-    assertTrue("this is a prime number!", PrimeCheck.isPrime(7);
-    assertTrue("this is a prime number!", PrimeCheck.isPrime(5179);
-    assertTrue("this is a prime number!", PrimeCheck.isPrime(3);
+    assertTrue("this is a prime number!", PrimeCheck.isPrime(7));
+    assertTrue("this is a prime number!", PrimeCheck.isPrime(5179));
+    assertTrue("this is a prime number!", PrimeCheck.isPrime(3));
   }
 
   // --------------------------------------------------------------- //
   @Test
   public void testNonPrimes() {
-    assertFalse("this is NOT a prime number!", PrimeCheck.isPrime(4);
-    assertFalse("this is NOT a prime number!", PrimeCheck.isPrime(55);
-    assertFalse("this is NOT a prime number!", PrimeCheck.isPrime(100000000);
+    assertFalse("this is NOT a prime number!", PrimeCheck.isPrime(4));
+    assertFalse("this is NOT a prime number!", PrimeCheck.isPrime(55));
+    assertFalse("this is NOT a prime number!", PrimeCheck.isPrime(100000000));
   }
 }
